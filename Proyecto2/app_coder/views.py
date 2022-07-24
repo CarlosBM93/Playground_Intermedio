4c41e8e63578be60ad6159424fa5c4b59a531d28
Carlos Buendia <cbuendia1984@gmail.com>
2022-07-24 00:26:26 -0500

    Inicio del proyecto!

---
+++ Proyecto2/app_coder/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from app_coder.models import Curso
+
+# Create your views here.
+
+def curso(self, nombre, camada):
+
+    curso = Curso(nombre=nombre, camada = camada)
+    curso.save()
+    return HttpResponse(f"""
+        <p>Curso: {curso.nombre} - Camada: {curso.camada} agregado</p>
+    """)
