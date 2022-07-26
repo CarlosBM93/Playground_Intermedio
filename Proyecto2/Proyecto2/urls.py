cdca8a8a4895b4b1895007f4037187382cedac84
Carlos Buendia <cbuendia1984@gmail.com>
2022-07-25 20:22:08 -0500

    Se añadio la linea de admin

--- Proyecto2/Proyecto2/urls.py
+++ Proyecto2/Proyecto2/urls.py
@@ -1,9 +1,10 @@
-
+from django.contrib import admin
 from django.urls import path,include
 
 from app_coder.views import curso
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('agrega-curso/<nombre>/<camada>/', curso),
     path('App-coder/', include('app_coder.urls')),
 ]
